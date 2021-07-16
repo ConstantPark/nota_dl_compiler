7a268c4af4ab34558b6a6ccbf60e1ff82cfc16c2
Constant Park <37441591+ConstantPark@users.noreply.github.com>
2021-07-16 14:32:26 +0900

    Add files via upload

---
+++ torch_script_transform.py
@@ -0,0 +1,13 @@
+import torch
+import torchvision
+
+# An instance of your model.
+model = torchvision.models.resnet18()
+
+# An example input you would normally provide to your model's forward() method.
+example = torch.rand(1, 3, 224, 224)
+
+# Use torch.jit.trace to generate a torch.jit.ScriptModule via tracing.
+traced_script_module = torch.jit.trace(model, example)
+torchscript_model_optimized = optimize_for_mobile(traced_script_module) 
+torchscript_model_optimized.save("model.pt")
